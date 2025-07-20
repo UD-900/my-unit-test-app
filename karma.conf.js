f8a3563cbf707a090ae790781855eb8804b71fbd
ardhik <utesting012@gmail.com>
2025-07-20 22:18:59 +0700

    new karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,6 @@
-// karma.conf.js
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -9,43 +11,54 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-junit-reporter') // <--- ADD THIS LINE
+      require('karma-junit-reporter') // <--- Add this line for JUnit reporting
     ],
     client: {
       jasmine: {
         // you can add configuration options for Jasmine here
-        // clearRunner: false,
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        defaultTimeoutInterval: 90000, // Increase timeout for CI environments if needed
+        stopSpecOnExpectationFailure: true,
+        random: false
       },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
-      suppressAll: true // Suppress all messages (overides other suppression settings)
+      suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/your-angular-app'),
+      dir: require('path').join(__dirname, './coverage/your-app-name'), // Replace 'your-app-name'
       subdir: '.',
       reporters: [
         { type: 'html' },
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml', 'junit'], // <--- ADD 'junit' HERE
+    reporters: ['progress', 'kjhtml', 'junit'], // <--- Add 'junit' here
+    junitReporter: { // <--- Configure JUnit reporter
+      outputDir: require('path').join(__dirname, './test-results'), // Directory for test reports
+      outputFile: 'junit-report.xml', // Name of the XML report file
+      suite: '', // Suite name (optional)
+      useBrowserName: false, // Do not add browser name to suite name
+      xmlVersion: '1' // XML report format version
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true, // This will be overridden by --no-watch in package.json for CI
-    browsers: ['Chrome'], // This will be overridden by --browsers=ChromeHeadless for CI
-    singleRun: false, // This will be overridden by --no-watch in package.json for CI
-    restartOnFileChange: true,
-
-    // <--- ADD THIS NEW SECTION FOR JUNIT REPORTER CONFIGURATION
-    junitReporter: {
-      outputDir: require('path').join(__dirname, './test-results'), // Path where the JUnit XML file will be saved
-      outputFile: 'junit.xml', // Name of the JUnit XML file
-      suite: '', // Optional: name for the test suite in the XML report
-      useBrowserName: false, // Optional: prevents adding browser name to suite name
-      xmlVersion: '1' // Optional: XML version for the report
-    }
-    // END OF NEW SECTION
+    autoWatch: false, // Set to false for CI/CD
+    browsers: ['ChromeHeadless'], // <--- Use ChromeHeadless for CI/CD
+    singleRun: true, // <--- Set to true for CI/CD
+    restartOnFileChange: false, // Set to false for CI/CD
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--no-sandbox', // Required for Jenkins in some environments
+          '--remote-debugging-port=9222',
+        ],
+      },
+    },
   });
 };
